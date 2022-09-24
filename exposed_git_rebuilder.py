837d27c3b037b7a0f27b6e41cc6db5d39f9ba818
overcookedtomato <yacine.saoudi@protonmail.com>
2022-09-24 12:10:18 -0400

    now working recursively no matter the amount of subdirectories + supporting proper arguments

--- exposed_git_rebuilder.py
+++ exposed_git_rebuilder.py
@@ -2,19 +2,15 @@
 
 import os
 import wget
+import argparse
 from subprocess import check_output
 from colorama import Fore, Back, Style
 from git import Repo
 
 
 
-
-
-
-
-#TODO: see how it works when merges are met
-#TODO: put retrieved files in properly named directory
-
+#unfortunately, the command git cat-file and git ls-tree don't seem implemented in gitpython, todo: someday creaye PR to add support natively
+#in the meantime, opening a subprocess works fine as long as git is installed
 def git_ls_tree(sha1):
     os.chdir('./rebuilt_repo')
     res = check_output(['git', 'ls-tree', sha1]).decode('utf-8')
@@ -27,12 +23,7 @@ def git_cat_file(sha1, arg):
     os.chdir('..')
     return res
 
-#unfortunately, the command git cat-file is not implementend in GitPython, todo: someday create PR to add support
-#in the meantime opening a subprocess works fine as long as git is installed
 def get_indexed_object_type(sha1):
-    #os.chdir('./rebuilt_repo')
-    #git_object_type = check_output(['git', 'cat-file', '-t', sha1]).decode('utf-8').strip()
-    #os.chdir('..')
     git_object_type = git_cat_file(sha1, "-t")
     return git_object_type
 
@@ -52,9 +43,7 @@ def get_git_object(sha1):
             os.mkdir("./rebuilt_repo/objects/" + git_object_index)
         
         os.rename("./" + git_object_name, "./rebuilt_repo/objects/" + git_object_index + "/" +  git_object_name)
-        #print(Fore.GREEN + "\nthe file was downloaded successfully and indexed into the rebuilt_repo") 
         print("\nthe file was downloaded successfully and indexed into the rebuilt_repo") 
-        #print(Style.RESET_ALL)
     
 
 def recursive_step(sha1, filename):
@@ -71,30 +60,23 @@ def recursive_step(sha1, filename):
         #author <sha1>
         #...
 
-        #We only need the hash from the first line :
         res =  git_cat_file(sha1, "commit")
         next_object_hash = res.partition('\n')[0].partition(' ')[2]
         next_object_type = res.partition('\n')[0].partition(' ')[0]
-        #print(Fore.GREEN + "This commit points to the tree " + next_object_hash) 
         print("This commit points to the " + next_object_type + ' '  + next_object_hash) 
-        #print(Style.RESET_ALL)
         
-        #TODO: tout ce que je viens de faire doit etre douable sans bricolage avec gitpython
-
-
-        #test = bare_repo.commit(sha1)
-
 
         print("\n---------------------------------------------------------------------------------------------------\n")
 
         if FOLLOW_PARENT_COMMIT:
-            #TODO: add log here
             recursive_step(next_object_hash, filename)
 
 
     elif git_object_type == 'tree':
-        #print("Building directory TODO XXXX")
-        #TODO mkdir but what the fuck is the name ?
+
+        if not os.path.exists(filename):
+            os.mkdir(filename)
+            print("directory " + filename + " has been built")
 
         print('\nthis tree contains :')
         tree_content = git_ls_tree(sha1)
@@ -124,13 +106,20 @@ def recursive_step(sha1, filename):
         print(Style.RESET_ALL)
 
 
+parser = argparse.ArgumentParser("python exposed_git_rebuilder")
+parser.add_argument("exposed_url", help="url of the exposed repository", type=str)
+parser.add_argument("last_commit_hash", help="sha1 of the last commit, can usually be found in . git/refs/heads/", type=str)
+args = parser.parse_args()
+print(args.exposed_url)
+
+EXPOSED_URL = args.exposed_url + '/objects'
+last_commit_hash = args.last_commit_hash
+
 #Creating a git repo to host our retrieved files:
 bare_repo = Repo.init(os.path.join(".", 'rebuilt_repo'), bare=True)
 assert bare_repo.bare
 
-FOLLOW_PARENT_COMMIT = True
-EXPOSED_URL = "http://127.0.0.1:80/.git/objects"
-first_commit_hash = "db660e15bcc1e4d4015c11eef40d517c2c34f947"
+FOLLOW_PARENT_COMMIT = True    #TODO: add a depth argument, a simple way to do it would be to count the '/' in the path then set this variable to false when depth is reached
 
-print("starting the reconstruction from the first known hash " + first_commit_hash + "\n")
-recursive_step(first_commit_hash, '.')
+print("starting the reconstruction from the first known hash " + last_commit_hash + "\n")
+recursive_step(last_commit_hash, '.')
